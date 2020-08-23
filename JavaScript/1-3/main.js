27ecda3bb9c2708e1648ee1b85eb6cc856a74c2c
Kyosuke-Kaneko <kyosuke.soccer118@gmail.com>
2020-08-23 23:49:18 +0900

    課題1ー3
    JavaScript条件分岐

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,19 @@
+// 問1
+var a
+a !== 10;
+var b
+b % 2 === 0 && b >=10 && b <= 20;
+// 問2
+let x = 10;
+if (x >= 10 && x <= 20 ) {
+  console.log("成功です");
+} else {
+  console.log("失敗です");
+}
+// 問3
+let y = 0;
+if (y % 2 == 0) {
+  console.log("偶数です");
+} else {
+  console.log("奇数です");
+}
