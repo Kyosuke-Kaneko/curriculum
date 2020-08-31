ca0135bdbaf0ab62c2582b89d5a5d50973e6c5a3
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2020-08-31 16:53:14 +0900

    課題2-5
    Java
    コレクションフレームワーク

--- JavaStudy/2-5/src/study/Study.java
+++ JavaStudy/2-5/src/study/Study.java
@@ -3,6 +3,7 @@ package study;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 /**
  * 本課題では、コレクションフレームワークのList系クラスとMap系クラスの基本的な記述を学びましょう。
@@ -20,6 +21,11 @@ public class Study {
     public static void main(String args[]) {
         // ① 定数を全て使って、String型のListを記述してください。
     	List<String> shohinList = new ArrayList<String>();
+    	shohinList.add(SHOP_SHOHIN_00);
+    	shohinList.add(SHOP_SHOHIN_01);
+    	shohinList.add(SHOP_SHOHIN_02);
+    	shohinList.add(SHOP_SHOHIN_03);
+
         // ② 以下の「shopMap.put(shohinList.get(1), 180);」の処理について、コメントを記述してください。
         /*
          * [ここへ記述]
@@ -32,8 +38,8 @@ public class Study {
         shopMap.put(shohinList.get(3), 100);
         // ③ カリキュラムを参考に拡張for文を使って、課題の画像と同じ表示になるよう記述してください。
         // 「shohinList」と「shopMap」が保持する値を上手く利用しましょう。
-        for (shohinList.Entry<String, Integer> entry: shopMap.entrySet()) {
-        	System.out.println(entry.getKey() + "＝" + entry.getValue() + "円になります！")；
+        for (Entry<String, Integer> entry: shopMap.entrySet()) {
+        	System.out.println(entry.getKey() + "＝" + entry.getValue() + "円になります！");
         }
     }
 
