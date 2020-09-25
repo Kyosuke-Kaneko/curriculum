7bb6b382aa06983e58de8fc3f9a0f9262cfa6a41
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2020-09-25 13:11:15 +0900

    Java2ー6
    途中まで

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,23 @@
+package check;
+
+public class Check {
+	private String firstName = "Kaneko";
+	private String lastName = "Kyosuke";
+
+//コンストラクター
+	public Check() {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	private void printName(String firstName, String lastName) {
+		System.out.println("printNameメソッド →" + this.firstName + this.lastName);
+	}
+
+	public static void main(String[] args) {
+		public pet = new Pet();
+		public constant = new Constants();
+
+
+	}
+
+}
