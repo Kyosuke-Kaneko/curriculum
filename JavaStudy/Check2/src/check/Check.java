adc6b02b6b0463694877c5ca1a2537d944cde1a6
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2020-09-28 03:29:14 +0900

    2章チェックテスト
    java

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,23 +1,27 @@
 package check;
 
+import constants.Constants; 
 public class Check {
-	private String firstName = "Kaneko";
-	private String lastName = "Kyosuke";
-
-//コンストラクター
-	public Check() {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-	private void printName(String firstName, String lastName) {
-		System.out.println("printNameメソッド →" + this.firstName + this.lastName);
-	}
 
 	public static void main(String[] args) {
-		public pet = new Pet();
-		public constant = new Constants();
-
+//		Constants consta = new Constants();
+		Pet pt = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+		RobotPet rp = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+		String firstName = "Kaneko";
+		String lastName = "Kyosuke";
+	    System.out.println("printNameメソッド→ " + printName(firstName, lastName));
+	    System.out.println();
+	    pt.introduce();
+	    System.out.println();
+	    rp.introduce();
 
 	}
+	private static String printName(String firstName, String lastName) {
+		String fullName = firstName + lastName;
+		return fullName;
+	}
+
+
 
 }
+
